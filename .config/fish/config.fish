3f5c270169c86ec8fc895c8b21ec866676b77709
Luis Belloch <luis@luisbelloch.es>
2018-02-05 09:15:23 +0100

    Better utf char for upstream-behind changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,7 +21,7 @@ set __fish_git_prompt_char_untrackedfiles '☡'
 set __fish_git_prompt_char_stashstate '↩ '
 set __fish_git_prompt_char_upstream_ahead '→'
 set __fish_git_prompt_char_upstream_equal '✓'
-set __fish_git_prompt_char_upstream_behind '⎌'
+set __fish_git_prompt_char_upstream_behind '⤺'
 
 function fish_prompt
   set last_status $status
