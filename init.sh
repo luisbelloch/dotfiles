db5abedc82fa181ced5f33a10eb23d5d6db70c38
Luis Belloch <luis@luisbelloch.es>
2018-11-15 22:18:42 +0100

    Added missing entries to init.sh file

--- init.sh
+++ init.sh
@@ -3,13 +3,17 @@ set -eu -o pipefail
 git submodule update --init --recursive
 readonly repo=$(pwd)
 pushd ~ > /dev/null
-ln -f -s $repo/.bin
 ln -f -s $repo/.bash_profile
 ln -f -s $repo/.bashrc
+ln -f -s $repo/.bin
+ln -f -s $repo/.config/fish/config.fish
+ln -f -s $repo/.fzf.bash
+ln -f -s $repo/.fzf.opts
+ln -f -s $repo/.inputrc
+ln -f -s $repo/.tigrc
+ln -f -s $repo/.tmux.conf
 ln -f -s $repo/.vim
 ln -f -s $repo/.vimrc
-ln -f -s $repo/.tmux.conf
-ln -f -s $repo/.config/fish/config.fish
 popd > /dev/null
 
 git config --global alias.tree "log --oneline --decorate --graph"
