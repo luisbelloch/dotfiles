8c59f7bbb686cd333acb00b5d8fee194d52a65fb
Luis Belloch <luis.belloch@flywire.com>
2024-06-29 09:08:04 +0200

    Missing global ignore file

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ ln -f -s $repo/.bashrc
 ln -f -s $repo/.bin
 ln -f -s $repo/.config/fish/config.fish
 ln -f -s $repo/.direnvrc
+ln -f -s $repo/.gitignore
 ln -f -s $repo/.fzf.bash
 ln -f -s $repo/.fzf.opts
 ln -f -s $repo/.inputrc
